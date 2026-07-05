3e3e057b594cfba6f290061b6a5890fac5c19fe6
Bugeenka <bugeenka@bugeenka.com>
2026-07-05 09:01:22 -0700

    moved desktop packages

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -3,6 +3,31 @@
 {
 
 
+## Packages
+environment.systemPackages = with pkgs; [
+
+  kitty
+  hyprlock
+  dunst
+  waybar
+  hyprlauncher
+  wl-clipboard
+  kdePackages.kio
+  kdePackages.kio-extras
+  kdePackages.kde-cli-tools
+  kdePackages.kservice
+  kdePackages.ark
+  mission-center
+  grim
+  slurp
+  hyprsysteminfo
+  thunar
+  thunar-volman
+  thunar-archive-plugin
+
+## Services
 
 
+
+];
 }
